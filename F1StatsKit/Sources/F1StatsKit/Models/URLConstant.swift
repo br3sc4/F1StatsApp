9e47463528bce615920e0882d58f26c8146749cd
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2023-03-22 18:29:23 +0100

    Created url factory

---
+++ F1StatsKit/Sources/F1StatsKit/Models/URLConstant.swift
@@ -0,0 +1,15 @@
+//
+//  URLConstants.swift
+//  
+//
+//  Created by Lorenzo Brescanzin on 22/03/23.
+//
+
+import Foundation
+
+public enum URLConstants: String {
+    case base = "https://ergast.com/api/f1/current"
+    case racesPath = ".json"
+    case driversStandings = "/driversStandings.json"
+    case constructorsStandings = "/constructorsStandings.json"
+}
