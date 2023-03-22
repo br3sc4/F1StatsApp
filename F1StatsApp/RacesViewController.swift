6135a65b8547329820c87ef854ffdcf3fd053ce1
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2023-03-22 18:57:24 +0100

    Setup for programmatic ui

--- F1StatsApp/RacesViewController.swift
+++ F1StatsApp/RacesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RacesViewController.swift
 //  F1StatsApp
 //
 //  Created by Lorenzo Brescanzin on 21/03/23.
@@ -7,13 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RacesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
