b684a525943bda6dbf0c2d2f9196a10ea09fc302
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2023-03-23 23:08:03 +0100

    Refactored endpoint creation as URL extension

---
+++ F1StatsKit/Sources/F1StatsKit/Extensions/URL+.swift
@@ -0,0 +1,26 @@
+//
+//  URLDirector.swift
+//  
+//
+//  Created by Lorenzo Brescanzin on 22/03/23.
+//
+
+import Foundation
+
+extension URL {
+    private static var baseURL: URL {
+        URL(string: "https://ergast.com/api/f1/current")!
+    }
+    
+    public static var races: URL {
+        baseURL.appending(path: ".json")
+    }
+    
+    public static var driversStanging: URL {
+        baseURL.appending(path: "/driversStandings.json")
+    }
+    
+    public static var constructorsStanding: URL {
+        baseURL.appending(path: "/constructorsStandings.json")
+    }
+}
